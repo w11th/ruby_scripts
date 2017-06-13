ef87d9fac23ebbedadb1d066377ef84beb1535b5
Wenhui Wang <w.eleventh@gmail.com>
2017-06-13 18:11:30 +0800

    add new script

--- helper.rb
+++ helper.rb
@@ -4,14 +4,20 @@ def print_memroy_usage
   memory_before = `ps -o rss= -p #{Process.pid}`.to_i
   yield
   memory_after = `ps -o rss= -p #{Process.pid}`.to_i
-
-  puts "Memroy: #{((memory_after - memory_before) / 1024).rount(2)} MB"
+  puts "Memroy: #{((memory_after - memory_before) / 1024).round(2)} MB"
 end
 
 def print_time_spent
   time = Benchmark.realtime do
     yield
   end
-
   puts "Time: #{time.round(2)} s"
+end
+
+def perform
+  print_memroy_usage do
+    print_time_spent do
+      yield
+    end
+  end
 end
